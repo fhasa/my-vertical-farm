0112608e05b3ee2c285a9d0bd729291578b53564
Fatima <fatimai.hasanat@gmail.com>
2023-01-02 21:35:41 +0200

    last edit

--- mqts.js
+++ mqts.js
@@ -18,16 +18,27 @@ function connectToMqtt() {
     message = new Paho.MQTT.Message("Hello");
     message.destinationName = "/World";
     client.send(message);
-    client.subscribe("iot_vertical_farming_ppu2022/centric/manual_big_pump1_water_flow");
-    client.subscribe("iot_vertical_farming_ppu2022/centric/manual_small_pump1_acid" );
-    client.subscribe("iot_vertical_farming_ppu2022/centric/manual_small_pump2_base");
-    client.subscribe("iot_vertical_farming_ppu2022/centric/manual_small_pump3_nutrient");
+    client.subscribe(
+      "iot_vertical_farming_ppu2022/centric/manual_big_pump1_water_flow"
+    );
+    client.subscribe(
+      "iot_vertical_farming_ppu2022/centric/manual_small_pump1_acid"
+    );
+    client.subscribe(
+      "iot_vertical_farming_ppu2022/centric/manual_small_pump2_base"
+    );
+    client.subscribe(
+      "iot_vertical_farming_ppu2022/centric/manual_small_pump3_nutrient"
+    );
     client.subscribe("iot_vertical_farming_ppu2022/centric/sensor_value");
     client.subscribe("iot_vertical_farming_ppu2022/branch1/manual_light");
-    client.subscribe("iot_vertical_farming_ppu2022/branch1/sensor_value");
+
     client.subscribe("iot_vertical_farming_ppu2022/branch2/manual_light");
     client.subscribe("iot_vertical_farming_ppu2022/branch2/sensor_value");
-    client.subscribe("iot_vertical_farming_ppu2022/RPI/update_firebase_range_value");
+    client.subscribe("iot_vertical_farming_ppu2022/branch1/sensor_value");
+    client.subscribe(
+      "iot_vertical_farming_ppu2022/RPI/update_firebase_range_value"
+    );
   }
 
   // called when the client loses its connection
@@ -39,11 +50,10 @@ function connectToMqtt() {
 
   // called when a message arrives
   function onMessageArrived(message) {
-    console.log("onMessageArrived:"+ message.topic + message.payloadString);
+    console.log("onMessageArrived:" + message.topic + message.payloadString);
     if (topics.includes(message.topic)) {
       console.log("includes");
       const obj = JSON.parse(message.payloadString, function (key, value) {
-
         let elem = document.getElementById(message.topic + key);
         if (elem != null) {
           elem.innerHTML = value;
@@ -66,8 +76,8 @@ function takeAction(myradio) {
 }
 
 function subscribe() {
-    sendMessage(
-      '{ "air_temperature": "30", "air_humidity": "52", "light_intensity": "900" }',
-      "iot_vertical_farming_ppu2022/branch2/sensor_value"
-    ); // code goes here
+  sendMessage(
+    '{ "air_temperature": "30", "air_humidity": "52", "light_intensity": "900" }',
+    "iot_vertical_farming_ppu2022/branch2/sensor_value"
+  ); // code goes here
 }
--- pages/api-test.js
+++ pages/api-test.js
@@ -1,11 +1,18 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-  
-import { getDatabase, ref ,get,child, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+import {
+  getDatabase,
+  ref,
+  get,
+  child,
+  query,
+  limitToLast,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 // import {Chart} from 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js'
 
 // Initialize Firebase
-const  firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyADywmfNsRaT-9RnE1MXS24nS5ujfgseBg",
   authDomain: "iot-vertical-farming-40e8a.firebaseapp.com",
   databaseURL: "https://iot-vertical-farming-40e8a-default-rtdb.firebaseio.com",
@@ -13,75 +20,102 @@ const  firebaseConfig = {
   storageBucket: "iot-vertical-farming-40e8a.appspot.com",
   messagingSenderId: "372914315084",
   appId: "1:372914315084:web:a609be0fc0f122561979e9",
-  measurementId: "G-8V3RS63HZW"
+  measurementId: "G-8V3RS63HZW",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
 // Initialize Realtime Database and get a reference to the service
 const database = getDatabase(app);
-/*
-const starCountRef = ref(db, 'posts/' + postId + '/starCount');
-onValue(starCountRef, (snapshot) => {
-  const data = snapshot.val();
-  updateStarCount(postElement, data);
+//bring last added element into web page
+const recentPostsRef = query(
+  ref(database, "sensor_value_history/branch1"),
+  limitToLast(1)
+);
+onValue(recentPostsRef, (snapshot) => {
+  const datan = snapshot.val();
+  console.log(datan);
 });
-*/
-// Get a reference to the database service
-
-// Set up the chart
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
-
-    // The data for our dataset
-    data: {
-        labels: [], // X-axis labels will be added dynamically
-        datasets: [{
-            label: 'My Data',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [] // Y-axis values will be added dynamically
-        }]
-    },
-
-    // Configuration options go here
-    options: {}
-});
-
-/*
-const starCountRef = ref(db, 'posts/' + postId + '/starCount');
-onValue(starCountRef, (snapshot) => {
-  const data = snapshot.val();
-  updateStarCount(postElement, data);
-});
-*/
 
 const dbRef = ref(getDatabase());
-get(child(dbRef, 'sensor_value_history/branch1')).then((snapshot) => {
-  if (snapshot.exists()) {
-    console.log(snapshot.val());
-    
-  } else {
-    console.log("No data available");
-  }
-}).catch((error) => {
-  console.error(error);
-});
+get(child(dbRef, "sensor_value_history/branch1"))
+  .then((snapshot) => {
+    if (snapshot.exists()) {
+      let xtemp = [];
+      let xhum = [];
+      let xdate = [];
+      let xlight = [];
+
+      const datajson = snapshot.val();
+
+      for (let key in datajson) {
+        // console.log(key);  // logs "object1", "object2", "object3"
+        xhum.push(datajson[key].air_humidity); // logs the object for each key
+        xtemp.push(datajson[key].air_temperature);
+        xdate.push(datajson[key].datetime);
+        xlight.push(datajson[key].light_intensity);
+      }
+
+      var ctx = document.getElementById("myChart").getContext("2d");
+      var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: "line",
+
+        // The data for our dataset
+        data: {
+          labels: xdate, // X-axis labels will be added dynamically
+          datasets: [
+            {
+              label: "Air Temperature",
+              backgroundColor: "rgb(255, 99, 132, .005)",
+              lineTension: 0,
+              borderColor: "rgb(255, 99, 132, 1)",
+              pointRadius: 1,
+              borderWidth: 1,
+              data: xtemp,
+              // Y-axis values will be added dynamically
+            },
+          ],
+        },
+        
+
+        // Configuration options go here
+        options: {
+
+          scales: {
+            yAxes: [{ ticks: { min: 0, max: 50 } }],
+            xAxes: [{
+              display: false  // hide the labels on the x-axis
+          }],
+        },
+          //   scales: {
+   
+          // }
+        },
+      });
+
+      // console.log(typeof(datajson));
+    } else {
+      console.log("No data available");
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+// database.ref('sensor_value_history/branch1').limitToLast(1).on("child_added", function(snapshot) {
+//   console.log(snapshot.val());
+// });
 
 // Add data to the chart when it is added to the database
-// const fatref=  ref(database,'sensor_value_history/branch1','/branch1');
+// const fatref=  ref(database,'sensor_value_history/branch1', limit(1));
 
 // onValue(fatref, function(snapshot) {
 //   const dta = snapshot.val();
 //   console.log(dta);
-//   // chart.data.labels.push(data.air_humidity);
-//   //console.log(data.branch1);
 
-//   chart.data.datasets[0].data.push(dta.air_temperature);
-//   console.log(dta.air_temperature);
-//   chart.update();
+//   // chart.data.datasets[0].data.push(dta.air_temperature);
+//   // console.log(dta.air_temperature);
+//   // chart.update();
 // });
