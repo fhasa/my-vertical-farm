de4654e64673ab35ea52fe800f90590ea4c1ac5f
Fatima <fatimai.hasanat@gmail.com>
2023-01-04 19:07:19 +0200

    adding charts logic

--- pages/api-test.js
+++ pages/api-test.js
@@ -28,17 +28,198 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Realtime Database and get a reference to the service
 const database = getDatabase(app);
-//bring last added element into web page
+
+
+
+// here last element in branch1 document
 const recentPostsRef = query(
   ref(database, "sensor_value_history/branch1"),
   limitToLast(1)
 );
 onValue(recentPostsRef, (snapshot) => {
-  const datan = snapshot.val();
-  console.log(datan);
+  const branch1last = snapshot.val();
+/*
+light_intensity
+datetime
+air_temperature
+air_humidity
+
+*/
+  for(let t in branch1last){
+    var humidity= branch1last[t].air_humidity;
+    document.getElementById("humidity1").innerHTML=humidity;
+    var airtemp1=branch1last[t].air_temperature;
+    document.getElementById('airt1').innerHTML=airtemp1;
+    var lightintensity1=branch1last[t].light_intensity;
+    document.getElementById('lightint1').innerHTML=lightintensity1;
+    var datebranch1=branch1last[t].datetime;
+    document.getElementById('datebr1').innerHTML=datebranch1;
+  }
+  console.log(branch1last);
 });
 
+
+
+
+
+
+
+
+
+
+// here last element in branch2 document
+const branch2 = query(
+  ref(database, "sensor_value_history/branch2"),
+  limitToLast(1)
+);
+onValue(branch2, (snapshot) => {
+  const br2data = snapshot.val();
+  console.log(br2data);
+});
+// here last element in centric document
+const water = query(
+  ref(database, "sensor_value_history/centric"),
+  limitToLast(1)
+);
+
+onValue(water, (snapshot) => {
+  const centricdata = snapshot.val();
+  
+  for(let m in centricdata){
+    var ph=centricdata[m].ph;
+    document.getElementById("ph").innerHTML = ph;
+    console.log(ph);
+    var tds=centricdata[m].tds;
+    document.getElementById("tds").innerHTML = tds;
+    console.log(tds);
+   var watt=centricdata[m].water_temperature;
+   document.getElementById("wtmp").innerHTML = watt;
+    console.log(watt);
+   var datt=centricdata[m].datetime;
+   document.getElementById("datec").innerHTML = datt;
+    console.log(datt);
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
 const dbRef = ref(getDatabase());
+
+get(child(dbRef, "sensor_value_history/centric"))
+  .then((snapshot) => {
+    const centricjson= snapshot.val();
+    let phm=[];
+    let tdsm=[];
+    let wtempm=[];
+    let cdatem=[];
+
+    for(let r in centricjson ){
+      phm.push(centricjson[r].ph);
+      tdsm.push(centricjson[r].tds);
+      wtempm.push(centricjson[r].water_temperature);
+      cdatem.push(centricjson[r].datetime);
+    }
+
+ // start of ph chart
+    var chrph = document.getElementById("myChart2").getContext("2d");
+   
+    var charph1= new Chart("myChart2", {
+        type: "line",
+        data: {
+            labels: cdatem,
+            datasets: [
+                {
+                    fill: false,
+                    lineTension: 0,
+                    backgroundColor: "rgba(0,255,0,.8)",
+                    borderColor: "rgba(0,255,0,0.1)",
+                    data: phm,
+                },
+            ],
+        },
+        options: {
+            legend: { display: false },
+            scales: {
+                yAxes: [{ ticks: { min: 0, max: 14 } }],
+            },
+        },
+    });
+                      
+//end of ph chart
+
+//start of tds chart
+
+var chrtds = document.getElementById("myChart1").getContext("2d");
+
+
+var chartds1= new Chart("myChart1", {
+  type: "line",
+  data: {
+      labels: cdatem,
+      datasets: [
+          {
+              fill: false,
+              lineTension: 0,
+              backgroundColor: "rgba(0,0,255,1.0)",
+              borderColor: "rgba(0,0,255,0.1)",
+              data: tdsm,
+          },
+      ],
+  },
+  options: {
+      legend: { display: false },
+      scales: {
+          yAxes: [{ ticks: { min: 100, max: 20000 } }],
+      },
+  },
+});
+
+//end of tds chart
+
+
+
+// start of water temp charts
+//end of water temp charts 
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 get(child(dbRef, "sensor_value_history/branch1"))
   .then((snapshot) => {
     if (snapshot.exists()) {
@@ -78,24 +259,23 @@ get(child(dbRef, "sensor_value_history/branch1"))
             },
           ],
         },
-        
 
         // Configuration options go here
         options: {
-
           scales: {
             yAxes: [{ ticks: { min: 0, max: 50 } }],
-            xAxes: [{
-              display: false  // hide the labels on the x-axis
-          }],
-        },
+            xAxes: [
+              {
+                display: false, // hide the labels on the x-axis
+              },
+            ],
+          },
           //   scales: {
-   
+
           // }
         },
       });
 
-      // console.log(typeof(datajson));
     } else {
       console.log("No data available");
     }
@@ -104,18 +284,3 @@ get(child(dbRef, "sensor_value_history/branch1"))
     console.error(error);
   });
 
-// database.ref('sensor_value_history/branch1').limitToLast(1).on("child_added", function(snapshot) {
-//   console.log(snapshot.val());
-// });
-
-// Add data to the chart when it is added to the database
-// const fatref=  ref(database,'sensor_value_history/branch1', limit(1));
-
-// onValue(fatref, function(snapshot) {
-//   const dta = snapshot.val();
-//   console.log(dta);
-
-//   // chart.data.datasets[0].data.push(dta.air_temperature);
-//   // console.log(dta.air_temperature);
-//   // chart.update();
-// });
